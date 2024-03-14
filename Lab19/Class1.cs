8a04a61ec79f55d14c89a3aeb08d5ec90457de7b
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-14 11:30:15 +0300

    Добавьте файлы проекта.

---
+++ Lab19/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19
+{
+    internal class Computer
+    {
+        private int brand_code;
+        private string brand_name;
+        private string processor_type;
+        private int processorfreq;
+        private int opmemory_volume;
+        private int disk_volume;
+        private int vidmemory_volume;
+        private int price;
+        private int number;
+
+        public int Brand_code { get { return brand_code; } set { brand_code = value; } }
+        public string Brand_name { get { return brand_name; } set { brand_name = value; } }
+        public string Processor_type { get { return processor_type; } set { processor_type = value; } }
+        public int Processorfreq { get {  return processorfreq; } set {  processorfreq = value; } }
+        public int Opmemory_volume { get {  return opmemory_volume; } set {  opmemory_volume = value; } }
+        public int Vidmemory_volume { get { return vidmemory_volume; } set { vidmemory_volume = value; } }
+        public int Disk_volume { get { return disk_volume; } set { disk_volume = value; } }
+        public int Price { get { return price; } set { price = value; } }
+        public int Number { get { return number; } set { number = value; } }
+        public Computer (int brand_code,  string brand_name, string processor_type, int processorfreq, int opmemory_volume, int disk_volume, int vidmemory_volume, int price, int number)
+        {
+            Brand_code=brand_code;
+            Brand_name= brand_name;
+            Processor_type= processor_type;
+            Processorfreq= processorfreq;
+            Opmemory_volume= opmemory_volume;
+            Disk_volume= disk_volume;
+            Vidmemory_volume= vidmemory_volume;
+            Price= price;
+            Number= number;
+
+        }
+       
+    }
+
+    }
+
---
+++ Lab19/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Lab19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Computer> comps = new List<Computer>()
+            {
+                new Computer(123,"Lenovo","type1",9900,228,64,1000,500000,50),
+                new Computer(456,"Apple","type2",1520,125,128,600,5000000,30),
+                new Computer(789,"Xiaomi","type1",9900,250,225,300,10000,1),
+                new Computer(124,"Lenovo","type3",5500,228,64,1000,150000,23),
+                new Computer(125,"Huawei","type2",14500,125,128,800,23000,43),
+                new Computer(126,"Apple","type3",8000,185,225,700,50000,56),
+                new Computer(123,"Kotik","type1",5500,1200,64,400,230000,5),
+
+            };
+            Print(comps);
+
+            Console.WriteLine("Введите название процессора ");
+            string name = Console.ReadLine();
+            List <Computer> comps2 = comps.Where(x=>x.Processor_type==name).ToList();
+            Print(comps2);
+
+
+            Console.WriteLine("Введите объем ОЗУ ");
+            int vol=Convert.ToInt32(Console.ReadLine());
+            List <Computer> comps3 = comps.Where (x=>x.Opmemory_volume>=vol).ToList();
+            Print(comps3);
+            Console.WriteLine();
+
+            List<Computer> comps4 = comps.OrderBy(x=>x.Price).ToList();
+            Print(comps4);
+            Console.WriteLine();
+
+            IEnumerable<IGrouping<string, Computer>> comps5 = comps.GroupBy(x=>x.Processor_type);
+
+            foreach (IGrouping<string, Computer> gr in comps5)
+            {
+                Console.WriteLine(gr.Key);
+                foreach (Computer c in gr)
+                {
+                    Console.WriteLine($"{c.Brand_code},{c.Brand_name},{c.Processor_type}, {c.Processorfreq},{c.Opmemory_volume},{c.Disk_volume},{c.Vidmemory_volume},{c.Price},{c.Number}");
+
+                }
+            }
+
+            Console.WriteLine();
+
+            Computer comp6=comps.OrderByDescending(x=>x.Price).FirstOrDefault();
+            Console.WriteLine($"{comp6.Brand_code},{comp6.Brand_name},{comp6.Processor_type}, {comp6.Processorfreq},{comp6.Opmemory_volume},{comp6.Disk_volume},{comp6.Vidmemory_volume},{comp6.Price},{comp6.Number}");
+            Console.WriteLine();
+            Computer comp7 = comps.OrderBy(x => x.Price).FirstOrDefault();
+            Console.WriteLine($"{comp7.Brand_code},{comp7.Brand_name},{comp7.Processor_type}, {comp7.Processorfreq},{comp7.Opmemory_volume},{comp7.Disk_volume},{comp7.Vidmemory_volume},{comp7.Price},{comp7.Number}");
+            Console.WriteLine();
+
+            
+
+            if (comps.Any(x=>x.Number>30)==true)
+            {
+                Console.WriteLine("есть");
+            }
+            else { Console.WriteLine("нет"); }
+            Console.ReadKey();
+
+        }
+
+        static void Print(List <Computer> computers)
+        {
+            foreach (Computer c in computers)
+            {
+
+                Console.WriteLine($"{c.Brand_code},{c.Brand_name},{c.Processor_type}, {c.Processorfreq},{c.Opmemory_volume},{c.Disk_volume},{c.Vidmemory_volume},{c.Price},{c.Number}");
+              
+
+            }
+            
+
+
+        }
+    }
+}
